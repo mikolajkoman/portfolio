039ebf12da5af536498a61d071db5942c47928e3
Mike <mikikoman@gamil.com>
2017-04-29 21:24:13 +0200

    update_to_view_2.00

--- form/send.php
+++ form/send.php
@@ -18,9 +18,7 @@ if (!isset($_POST['contact_body']) || empty($_POST['contact_body'])) {
     $error = true;
 }
 
-if (!isset($_POST['contact_check']) || empty($_POST['contact_check']) || (int) $_POST['contact_check'] !== ((int) $_POST['contact_check_data'][0] * (int) $_POST['contact_check_data'][1])) {
-    $error = true;
-}
+
 
 if ($error) {
     exit('nok');
