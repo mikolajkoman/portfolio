804e8590fb0e878bf2cbef9a561380220fa93a1b
Mike <mikikoman@gamil.com>
2017-06-15 09:29:51 +0200

    next steps to up

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
     $(document).ready(function(){
-        $(document.body).fadeIn(3000);
+        $(document.body).fadeIn(6000);
         // $(".slogan").fadeIn(10000);
 //start animation    
 
@@ -8,10 +8,53 @@
  // dis.classList.add("disabled");
  // add disable class for en lang for this time
 
-    var about = document.querySelector("#about"),
-        about1 = document.querySelector("#about1"),
-        card = document.querySelector(".card"),
-        button  =   card.querySelector("button");
+    var card = document.querySelector(".card"),
+        row = document.querySelector(".row"),
+        button  =   card.querySelector("button"),
+        buttonB  =   row.querySelector("button"),
+        article  =   card.querySelector("article");
+
+function createButton(){
+    var button1 = document.createElement("button");
+         i = document.createElement("i");
+
+    button1.classList.add("backToTop", "hidden");
+    
+    document.body.appendChild(button1);
+    button1.appendChild(i);
+    i.style.fontSize = "40px";
+    i.classList.add("fa", "fa-chevron-circle-up");
+    // button1.addEventListener("mouseover",  function () {
+    //     button1.textContent = "Do góry" ;
+    // });
+    // button1.addEventListener("mouseout",  function () {
+    //     button1.textContent = " " ;
+    //     button1.appendChild(i);
+    // });
+    return button1;
+}
+var button1 = createButton();
+
+function animateScroll() {
+    if (document.body.scrollTop > 0) {
+        window.scrollBy(0, -5);
+        setTimeout(animateScroll, 10);
+    } 
+}
+
+button1.addEventListener("click", function (e) {
+    // e.stopPropagation();
+    animateScroll();
+}, false);
+window.addEventListener("scroll", function (e) {
+    if(document.body.scrollTop >= 100){
+        button1.classList.remove("hidden");
+    }
+    else {
+        button1.classList.add("hidden");
+    }
+}, false);
+
 
 $( window ).scroll(function scrollAnimations() {
     var scrollA = document.body.scrollTop;
@@ -21,7 +64,7 @@ $( window ).scroll(function scrollAnimations() {
         naviStyle.backgroundColor = "rgba(0, 0, 0, 0.6)";
         naviStyle.boxShadow = "-3px 3px 3px 0 rgba(0, 0, 0, 0.9)";
         naviStyle.transform = "scale(1,0.8)";
-        naviStyle.top = "-5px";
+        naviStyle.top = "-10px";
 
     }
     else{
@@ -32,13 +75,8 @@ $( window ).scroll(function scrollAnimations() {
         naviStyle.top = null;
     }
   if ( 600 <= scrollA && scrollA <= 1000) {
-        button.addEventListener("click", function(){
-        about1.classList.add("hidden");
-        about.classList.remove("hidden");
-        $(".diva").animate({left: '-200px'}, (1000)),
-        document.querySelector(".diva1").style.backgroundColor = "rgba(0, 0, 0, 0.6)",
-        $(".diva1").fadeIn(3600);});
-        return 0;
+        
+        $(".card").fadeIn(3600);
     }
       if ( 855 <= scrollA && scrollA ) {
 
@@ -48,8 +86,32 @@ $( window ).scroll(function scrollAnimations() {
         $(".want").fadeIn(8400);
         return 0;
     }
+    button.addEventListener("click", function(){
+        article.classList.add("hidden");
+        row.classList.remove("hidden");
+        $(".diva").fadeIn(3600),
+        document.querySelector(".diva1").style.backgroundColor = "rgba(0, 0, 0, 0.6)",
+        $(".diva1").fadeIn(3600);
+            buttonB.addEventListener("click", function(){
+                row.classList.add("hidden");
+                article.classList.remove("hidden");
+                $(".article").fadeIn(3600);
+                function animateScrolli() {
+                    if (document.body.scrollTop > 900) {
+                        window.scrollBy(0, -5);
+                        setTimeout(animateScrolli, 20);
+                    } 
+                }
+                animateScrolli();
 
+            });
+    });
+    return 0;
 });
+
+
+
+
 
 $(function myContactForm() {
     var check_1 = (Math.floor(Math.random() * 9) + 1).toString(),
